c5b7e79291b45fc419962131ab446e5732bf2a8b
Tejas <tejassinghsital@gmail.com>
2022-04-10 08:49:54 +0530

    CReated prenavbar of mi clone website using react js till now

---
+++ first-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Image
+      Name
+      Price
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import PreNavBar from './Components/PreNavBar';
+
+function App() {
+  return (
+    <>
+      <PreNavBar />
+
+    </>
+
+
+
+  );
+}
+
+export default App;
